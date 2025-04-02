fa233bd0106d23ed20f9b4b00c5f7b889c1098d2
StudyingAnt <61408144+StudyingAnt@users.noreply.github.com>
2025-04-02 19:25:49 +0000

    Deploying to gh-pages from @ sbu-damaghi-ceel/sbu-damaghi-ceel.github.io@52c84f34da8d780eda278d5cc3200713cd91e607 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,51 +379,26 @@ ninja.data = [{
             },},{id: "news-new-ceel-webpage-released",
           title: 'New CEEL webpage released!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-project-1-ecology-and-evolution-of-breast-carcinogenesis",
+          title: 'Project 1: Ecology and Evolution of Breast Carcinogenesis',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-project-2-metabolic-phenotypes-present-in-dcis-to-stratify-disease-progression-and-upstaging",
+          title: 'Project 2: Metabolic phenotypes present in DCIS to stratify Disease progression and upstaging...',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-project-3-co-evolution-of-tumor-and-stroma-in-breast-cancer",
+          title: 'Project 3: Co-evolution of tumor and stroma in breast cancer',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-project-4-evolution-of-resistant-phenotype-to-parpi-in-ovarian-cancer-genotypic-heterogeneity-vs-phenotypic-plasticity",
+          title: 'Project 4: Evolution of resistant phenotype to PARPi in ovarian cancer: genotypic heterogeneity...',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
